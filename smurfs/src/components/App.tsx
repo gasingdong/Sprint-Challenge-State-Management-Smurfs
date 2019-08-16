d482dbff636cbe25918e38aa193fae6fe8b04b3f
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-16 09:13:36 -0700

    use context provider

--- smurfs/src/components/App.tsx
+++ smurfs/src/components/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
+import { VillageProvider } from '../store/context';
 
 const App = (): React.ReactElement => {
   return (
-    <div className="App">
-      <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
-    </div>
+    <VillageProvider>
+      <div className="App">
+        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <div>Welcome to your state management version of Smurfs!</div>
+        <div>Start inside of your `src/index.js` file!</div>
+        <div>Have fun!</div>
+      </div>
+    </VillageProvider>
   );
 };
 
