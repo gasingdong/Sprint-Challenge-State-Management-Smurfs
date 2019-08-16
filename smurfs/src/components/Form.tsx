d666d962e791c5c1962185e54a340a1545af3b35
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-16 10:29:26 -0700

    sync form to state

--- smurfs/src/components/Form.tsx
+++ smurfs/src/components/Form.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = (): React.ReactElement => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [height, setHeight] = useState('');
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+  };
+
+  const nameHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setName(event.currentTarget.value);
+  };
+
+  const ageHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setAge(event.currentTarget.value);
+  };
+
+  const heightHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setHeight(event.currentTarget.value);
+  };
+
   return (
     <div className="new-resident-form">
-      <form>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="age" placeholder="Age" />
-        <input type="text" name="height" placeholder="Height" />
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={nameHandler}
+        />
+        <input
+          type="text"
+          name="age"
+          placeholder="Age"
+          value={age}
+          onChange={ageHandler}
+        />
+        <input
+          type="text"
+          name="height"
+          placeholder="Height"
+          value={height}
+          onChange={heightHandler}
+        />
+        <button type="submit">Add Resident</button>
       </form>
     </div>
   );
