f07f0da90eb5f98e9aaffc4d8a44fb6a21788998
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-16 10:12:17 -0700

    render village residents to list

--- smurfs/src/components/VillageResidents.tsx
+++ smurfs/src/components/VillageResidents.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { useVillageContext } from '../store/context';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import Resident from './Resident';
+import { useVillageContext } from '../store/context';
+import { fetchSmurfs, successSmurfs, failSmurfs } from '../store/actions';
 
 const VillageResidents = (): React.ReactElement => {
-  const { smurfs } = useVillageContext()[0];
+  const [state, dispatch] = useVillageContext();
+  const { smurfs } = state;
+
+  useEffect((): void => {
+    const fetch = async (): Promise<void> => {
+      dispatch(fetchSmurfs());
+      try {
+        const response = await axios.get('http://localhost:3333/smurfs');
+        console.log('response', response);
+        dispatch(successSmurfs(response.data));
+      } catch (error) {
+        dispatch(failSmurfs());
+      }
+    };
+    fetch();
+  }, [dispatch]);
+
   return (
     <div className="residents">
       {smurfs.map(
