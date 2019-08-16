65153d7c444e04565fd6a6da31f05f0a10dfccb8
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-16 08:39:46 -0700

    refactor to typescript

---
+++ smurfs/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "parser":  "@typescript-eslint/parser",
+  "extends": [
+    "airbnb-typescript",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+	  "plugin:prettier/recommended",
+  ],
+  "parserOptions": {
+	  "ecmaVersion": 6,
+	  "sourceType": "module",
+	  "ecmaFeatures": {
+      "jsx": true
+    },
+    "extraFileExtensions": [".vue"],
+  },
+  "env": { 
+    "es6": true,
+  },
+  "globals": {
+    "console": "readonly",
+	  "document": "readonly",
+	  "window": "readonly",
+  },
+  "settings": {
+	"react": {
+	  "version": 'detect',
+	  }
+  }
+};
---
+++ smurfs/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = (): React.ReactElement => {
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>Have fun!</div>
+    </div>
+  );
+};
+
+export default App;
